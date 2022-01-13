5c2720fcd602b52ef95c2ae4e32bf5d65d3c955e
utsubyo <97504303+15-junyuan@users.noreply.github.com>
2022-01-13 11:51:09 +0800

    unity score
    add score text
    when healthy item collect add 10point

--- 15_junyuan_Lab3/Assets/Scripts/BasketMovementScript.cs
+++ 15_junyuan_Lab3/Assets/Scripts/BasketMovementScript.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BasketMovementScript : MonoBehaviour
 {
     public float speed;
+    int Score;
 
-    
+    public Text Scoretxt;
     
    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Scoretxt.text = "Score: " + Score;
     }
 
     // Update is called once per frame
@@ -35,9 +38,14 @@ public class BasketMovementScript : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter(Collision other)
     {
-        
+        if (other.gameObject.tag == "Healthy")
+        {
+            Score =+ Score + 10;
+            Scoretxt.text = "Score: " + Score;
+            Destroy(other.gameObject);
+        }
     }
 
 
